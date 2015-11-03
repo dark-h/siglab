32d4bfff56240cf1575fe3b4de38ecbe686bfc24
dark-h <dark-h@nowhere.com>
2015-11-03 23:33:20 +0000

    playing with seesaw

--- src/gui/clojure/main-frame.clj
+++ src/gui/clojure/main-frame.clj
@@ -1,4 +1,22 @@
 (ns siglab.gui)
 (use '(seesaw core))
 
-(show! (frame :title "Siglab", :size [640 :by 480] :on-close :exit, :content "Welcome to siglab"))
+(def f (frame :title "Siglab", :size [640 :by 480] :on-close :exit))
+
+(defn display [content]
+  (config! f :content content)
+  content)
+
+(def bp (border-panel :hgap 50 :vgap 10
+  :center "CENTER"
+  :north  "NORTH"
+  :south  "SOUTH"
+  :east   "EAST"
+  :west   "WEST"))
+
+;(display bp)
+(def p1 (abstract-panel :color "red"))
+(def p2 (abstract-panel :color "green"))
+(def p3 (abstract-panel :color "blue"))
+(display p1)
+(show! f)
